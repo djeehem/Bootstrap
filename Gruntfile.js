a42503ed72f648599415636394a8d197eb6ac6fe
djeehem <djeehem@gmail.com>
2019-11-20 18:41:14 -0500

    Gulp Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,9 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
 
-    require('jit-grunt')(grunt, {
-    	useminPrepare: 'grunt-usemin'
-    });
+	require('jit-grunt')(grunt, {
+	  useminPrepare: 'grunt-usemin'
+	});
     
     grunt.initConfig({
         sass: {
@@ -19,69 +19,69 @@ module.exports = function (grunt) {
         },
 
         copy: {
-        	html: {
-        		files: [
-        		{
-        			expand: true,
-        			dot: true,
-        			cwd: './',
-        			src: ['*.html'],
-        			dest: 'dist'
-        		}]
-        	},
-        	fonts: {
-        		files: [
-        		{
-        			expand: true,
-        			dot: true,
-        			cwd: 'node_modules/font-awesome',
-        			src: ['fonts/*.*'],
-        			dest: 'dist'
-        		}]
-        	}
+            html: {
+                files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]                
+            },
+            fonts: {
+                files: [
+                {
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
         },
 
         imagemin: {
-        	dynamic: {
-        		files: [{
-        			expand: true,
-        			cwd: './',
-        			src: ['img/*.{png,jpg,gif}'],
-        			dest: 'dist'
-        		}]
-        	}
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
+            }
         },
         
         clean: {
-        	build: {
-        		src: ['dist/']
-        	}
+            build: {
+                src: [ 'dist/']
+            }
         },
 
         useminPrepare: {
-        	foo: {
-        		dest: 'dist',
-        		src: ['contactus.html', 'aboutus.html', 'index.html']
-        	},
-        	options: {
-        		flow: {
-        			steps: {
-        				css: ['cssmin'],
-        				js: ['uglify']
-        			},
-        			post: {
-        				css: [{
-        					name: 'cssmin',
-        					createConfig: function(context, block) {
-        						var generated = context.options.generated;
-        							generated.options = {
-        								keepSpecialComments: 0, rebase: false
-        							};
-        					}
-        				}]
-        			}
-        		}
-        	}
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
+                    }
+                }
+            }
         },
 
         concat: {
